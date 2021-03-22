6b076fe60d6a69fc9feb15e4deddf65e85243209
GromZDev <Dmitryev@mail.ru>
2021-03-22 03:04:11 +0300

    Сделал активити на котлин, организовал в нём фрагмент. И вставил кнопку с выводом текста на котлин.

--- app/src/main/java/com/example/kotlin_lesson_1/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_lesson_1/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.kotlin_lesson_1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            val fragment: Fragment = MainFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
+
     }
 }
