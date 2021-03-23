0f6a86b8beea7a88a319de7c20c2dfaa16e8f085
GromZDev <Dmitryev@mail.ru>
2021-03-24 00:46:30 +0300

    Создал во ViewModel LiveData чтобы не хранить ссылки на Fragment во ViewModel. LiveData - реализация паттерна Observable.

--- app/src/main/java/com/example/kotlin_lesson_1/MainFragment.kt
+++ app/src/main/java/com/example/kotlin_lesson_1/MainFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainFragment : Fragment() {
@@ -35,7 +37,33 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+// ViewModelProvider отвечает за создание и сохранение ViewModel. Можно получить
+//ViewModel, вызвав метод get(Class modelClass) того класса, в который передаём класс,
+//унаследованный от ViewModel.
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+// LiveData представляет собой реализацию паттерна Observable. С помощью метода observe() мы можем
+// подписаться на изменения в LiveData и получать обновлённые данные каждый раз, когда вызван
+// один из методов для передачи данных в эту LiveData. Подписка на изменения LiveData из Activity
+// выглядит так:
+        val observer = Observer<Any> {renderData(it)}
+
+// Метод observe() принимает два параметра. 1 LifecycleOwner — интерфейс, который реализуют
+// AppCompatActivity и Fragment из библиотеки поддержки. С его помощью можно получить объект
+// Lifecycle, а через него — callback при изменении состояния Activity или Fragment. 2 Экземпляр
+// класса, реализующего интерфейс Observer, который в методе onChanged() принимает данные из
+// LiveData. Когда подписываемся на неё, она всегда знает о состоянии Activity или Fragment.
+// Метод onChanged() вызывается, только если Activity или Fragment активны: вызван метод
+// onStart(), но не вызван onStop(). То есть LiveData всегда знает благодаря LifecycleOwner,
+// жива наша Activity или нет, чтобы не передавать данные в уничтоженную Activity.
+
+        viewModel.getLiveData().observe(viewLifecycleOwner, observer)
+
+    }
+
+    private fun renderData(data: Any) {
+            Toast.makeText(context, "data", Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/com/example/kotlin_lesson_1/MainViewModel.kt
+++ app/src/main/java/com/example/kotlin_lesson_1/MainViewModel.kt
@@ -1,7 +1,23 @@
 package com.example.kotlin_lesson_1
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel: ViewModel() {
+// 1 Это класс, который может переживать пересоздание активити, -> сохраняет все модели с данными
+// 2 Класс ViewModel может и должен хранить все экземпляры LiveData, чтобы они переживали
+// пересоздание Activity
+// 3 Связь ViewModel с View осуществляется через LiveData, то есть ViewModel не управляет View
+//явным образом
+// 4 передаём в конструктор реализацию LiveData, поскольку сам класс LiveData абстрактный
+class MainViewModel(
+    private val liveDataToObserve: MutableLiveData<Any> =
+        MutableLiveData()
+) : ViewModel() {
+
+    fun getLiveData(): LiveData<Any> {
+        return liveDataToObserve
+    }
+
 
 }
