ef65bbadb528a902a2e13986eb5b4a54e8b11c06
GromZDev <Dmitryev@mail.ru>
2021-03-26 17:36:48 +0300

    В классе состояния приложения теперь переменная cinemaData - это список из FilmFeature. А в ViewModel теперь логика возвращения списка такая: в метод передается булево, и если это не список всех фильмов, то берем популярные фильмы. И наоборот.

--- app/src/main/java/com/example/kotlin_lesson_1/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/kotlin_lesson_1/viewModel/MainViewModel.kt
@@ -7,12 +7,6 @@ import com.example.kotlin_lesson_1.model.Repository
 import com.example.kotlin_lesson_1.model.RepositoryImpl
 import java.lang.Thread.sleep
 
-// 1 Это класс, который может переживать пересоздание активити, -> сохраняет все модели с данными
-// 2 Класс ViewModel может и должен хранить все экземпляры LiveData, чтобы они переживали
-// пересоздание Activity
-// 3 Связь ViewModel с View осуществляется через LiveData, то есть ViewModel не управляет View
-//явным образом
-// 4 передаём в конструктор реализацию LiveData, поскольку сам класс LiveData абстрактный
 class MainViewModel(
     private val liveDataToObserve: MutableLiveData<AppState> =
         MutableLiveData(),
@@ -21,18 +15,19 @@ class MainViewModel(
 
     fun getLiveData() = liveDataToObserve // Получаем LiveData. Она хранит состояние приложения
 
-    fun getFilmFromLocalSource() = getDataFromLocalSource() // Получаем данные
+    fun getFilmFromLocalSourceAllFilms() = getDataFromLocalSource(isAllFilms = true) // Получаем данные
 
-    fun getFilmFromRemoteSource() = getDataFromLocalSource()
-    // Метод, который имитирует запрос к БД или ещё какому-то источнику данных в приложении. Запрос
-// осуществляется асинхронно в отдельном потоке. Как только поток просыпается, мы передаём в нашу
-// LiveData какие-то данные через метод postValue. Если данные передаются в основном потоке,
-// используйте метод setValue.
-    private fun getDataFromLocalSource() {
+    fun getFilmFromLocalSourcePopularFilms() = getDataFromLocalSource(isAllFilms = false)
+
+    fun getFilmFromRemoteSource() = getDataFromLocalSource(isAllFilms = true)
+
+    private fun getDataFromLocalSource(isAllFilms: Boolean) {
         liveDataToObserve.value = AppState.Loading
         Thread {
-            sleep(3000)
-            liveDataToObserve.postValue(AppState.Success(repositoryImpl.getFilmFromLocalStorage()))
+            sleep(3000) // Теперь берем данные из списков:
+            liveDataToObserve.postValue(AppState.Success(if (isAllFilms)
+                repositoryImpl.getFilmFromLocalStorageAllFilms() else
+                repositoryImpl.getFilmFromLocalStoragePopularFilms()))
         }.start()
     }
 
