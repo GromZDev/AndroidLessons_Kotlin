c3ca39bbecceaeff88c2cf0a067706405376f6c0
GromZDev <Dmitryev@mail.ru>
2021-03-28 00:30:55 +0300

    В конструктор фильма добавил его длительность.

--- app/src/main/java/com/example/kotlin_lesson_1/model/Film.kt
+++ app/src/main/java/com/example/kotlin_lesson_1/model/Film.kt
@@ -1,5 +1,9 @@
 package com.example.kotlin_lesson_1.model
 
-data class Film (val filmName: String, val filmImage: Int, val filmRating: Double, val filmYear: Int
+data class Film (val filmName: String,
+                 val filmImage: Int,
+                 val filmRating: Double,
+                 val filmYear: Int,
+                 val filmTime: Int
 
 )
