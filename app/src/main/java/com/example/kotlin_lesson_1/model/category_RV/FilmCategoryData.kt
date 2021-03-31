38153acb05e9f472662ba33bd9559c691aa3dd3e
GromZDev <Dmitryev@mail.ru>
2021-03-31 12:07:32 +0300

    Переделал логику выбора списка фильмов в двойном ресайклере.

--- app/src/main/java/com/example/kotlin_lesson_1/model/category_RV/FilmCategoryData.kt
+++ app/src/main/java/com/example/kotlin_lesson_1/model/category_RV/FilmCategoryData.kt
@@ -1,5 +1,7 @@
 package com.example.kotlin_lesson_1.model.category_RV
 
+import com.example.kotlin_lesson_1.model.getAllFilms
+import com.example.kotlin_lesson_1.model.getDefaultFilm
 import com.example.kotlin_lesson_1.model.getPopularFilms
 import java.util.*
 
@@ -7,19 +9,41 @@ object FilmCategoryData {
 
     private val random = Random()
 
-    private val titles =  arrayListOf( "Star Wars Films", "Marvel's Films", "Top Films", "Comedy Films", "Action Films")
+    private val titles = arrayListOf(
+        "Star Wars Films",
+        "Marvel's Films",
+        "Top Films",
+        "Comedy Films",
+        "Action Films"
+    )
 
-    private fun randomTitle() : String{
+    private fun randomTitle(): String {
         val index = random.nextInt(titles.size)
         return titles[index]
     }
 
-    fun getParents(count : Int) : List<FilmSpecificListCategory>{
+//    fun getParents(count : Int) : List<FilmSpecificListCategory>{
+//        val parents = mutableListOf<FilmSpecificListCategory>()
+//        repeat(count){
+//            val parent = FilmSpecificListCategory(randomTitle(), getPopularFilms())
+//            parents.add(parent)
+//        }
+//        return parents
+//    }
+
+    fun getParents(count: Int): List<FilmSpecificListCategory> {
         val parents = mutableListOf<FilmSpecificListCategory>()
-        repeat(count){
-            val parent = FilmSpecificListCategory(randomTitle(), getPopularFilms())
-            parents.add(parent)
+        var listOfFilms: FilmSpecificListCategory
+
+        for (i in 1..count) {
+            if (i % 2 == 0) {
+                listOfFilms = FilmSpecificListCategory(randomTitle(), getPopularFilms())
+                parents.add(listOfFilms)
+            } else listOfFilms = FilmSpecificListCategory(randomTitle(), getAllFilms())
+            parents.add(listOfFilms)
         }
         return parents
     }
+
+
 }
