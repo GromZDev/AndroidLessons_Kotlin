254667c3996f084f41418d4a8366e3e04c1a4930
GromZDev <Dmitryev@mail.ru>
2021-03-31 10:20:12 +0300

    Прокинул слушатель нажатия в адаптер нового ресайклера, в функции bind его сетим. А в onBindViewHolder присваиваем к конкретному элементу.
    Также в адаптере категорий фильмов (FilmCategoryAdapter), которому "принадлежит" адаптер конкретной категории (SpecificFilmAdapter), инициируем fragmentManager и передаем его для создания транзакции перехода во фрагмент описания.

--- app/src/main/java/com/example/kotlin_lesson_1/view/category_RV/SpecificFilmAdapter.kt
+++ app/src/main/java/com/example/kotlin_lesson_1/view/category_RV/SpecificFilmAdapter.kt
@@ -8,15 +8,18 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlin_lesson_1.R
 import com.example.kotlin_lesson_1.model.FilmFeature
+import com.example.kotlin_lesson_1.view.MainFilmFragment
 import kotlinx.android.synthetic.main.item_category_film_recyclerview.view.*
 
-class SpecificFilmAdapter (private val specificFilm: List<FilmFeature>) :
-        RecyclerView.Adapter<SpecificFilmAdapter.ViewHolder>(){
-
+class SpecificFilmAdapter(
+    private val specificFilm: List<FilmFeature>, private var onItemViewClickListener:
+    MainFilmFragment.OnItemViewClickListener?
+) :
+    RecyclerView.Adapter<SpecificFilmAdapter.ViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        var view = LayoutInflater.from(parent.context)
+        val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_category_film_recyclerview, parent, false)
         return ViewHolder(view)
     }
@@ -25,15 +28,28 @@ class SpecificFilmAdapter (private val specificFilm: List<FilmFeature>) :
         val currentFilm = specificFilm[position]
         holder.imageView.setImageResource(currentFilm.film.filmImage)
         holder.textView.text = currentFilm.film.filmName
+
+        holder.bind(specificFilm[position])
     }
 
     override fun getItemCount(): Int {
         return specificFilm.size
     }
 
-    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var textView: TextView = itemView.child_textView
         var imageView: ImageView = itemView.film_category_imageView
+
+        fun bind(films: FilmFeature) {
+            itemView.setOnClickListener {
+                onItemViewClickListener?.onItemViewClick(films) // Вызываем слушатель нажатия
+            }
+        }
+
+    }
+
+    fun removeListener() {
+        onItemViewClickListener = null
     }
 
 }
