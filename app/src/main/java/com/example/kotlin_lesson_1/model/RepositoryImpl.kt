ca3770c2c9d57f55286887f382ef8bad0d42fa3a
GromZDev <Dmitryev@mail.ru>
2021-04-03 00:29:58 +0300

    Теперь возвращаем сразу функцию в RepositoryImpl.kt. А в адаптерах возвращаем сразу значение.

--- app/src/main/java/com/example/kotlin_lesson_1/model/RepositoryImpl.kt
+++ app/src/main/java/com/example/kotlin_lesson_1/model/RepositoryImpl.kt
@@ -2,20 +2,11 @@ package com.example.kotlin_lesson_1.model
 
 // Имплементация интерфейса Repo
 class RepositoryImpl: Repository {
-    override fun getFilmFromLocalStorage(): FilmFeature {
-        return FilmFeature(getDefaultFilm(),"Самый кассовый фильм, с топовыми актерами", "Роберт Дауни Jr")
-    }
-
-    override fun getFilmFromServer(): FilmFeature {
-        return FilmFeature(getDefaultFilm(), "Самый кассовый фильм, с топовыми актерами", "Роберт Дауни Jr")
-    }
 
 // Имплементируем методы интерфейса, и в возврате делаем соответствующий список
-    override fun getFilmFromLocalStorageAllFilms(): List<FilmFeature> {
-        return getAllFilms()
-    }
+    override fun getFilmFromLocalStorageAllFilms(): List<FilmFeature> = getAllFilms()
 
-    override fun getFilmFromLocalStoragePopularFilms(): List<FilmFeature> {
-        return getPopularFilms()
-    }
+
+    override fun getFilmFromLocalStoragePopularFilms(): List<FilmFeature> = getPopularFilms()
+
 }
