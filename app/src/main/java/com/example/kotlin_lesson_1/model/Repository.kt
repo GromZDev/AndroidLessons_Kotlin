0b6c6b7f649f492bb81e9ea92e477278e64dc7f8
GromZDev <Dmitryev@mail.ru>
2021-04-03 00:22:11 +0300

    Убрал ненужный код.

--- app/src/main/java/com/example/kotlin_lesson_1/model/Repository.kt
+++ app/src/main/java/com/example/kotlin_lesson_1/model/Repository.kt
@@ -2,8 +2,7 @@ package com.example.kotlin_lesson_1.model
 
 // Интерфейс репозитория
 interface Repository {
-    fun getFilmFromServer(): FilmFeature
-    fun getFilmFromLocalStorage(): FilmFeature
+
     fun getFilmFromLocalStorageAllFilms(): List<FilmFeature>
     fun getFilmFromLocalStoragePopularFilms(): List<FilmFeature>
 }
