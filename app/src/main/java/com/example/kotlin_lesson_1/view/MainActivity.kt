ddfe50d6c8205a752abc62f7ca4515a4ccf14082
GromZDev <Dmitryev@mail.ru>
2021-04-09 04:26:18 +0300

    Сделал класс широковещательного сообщения - MySystemBroadcastReceiver. Здесь имплементируем один метод onReceive, в нём написал логику отображения трёх системных сообщений: - от смены языка (через манифест) - от изменения режима полёта (программно) - от изменения wifi подключения (программно)
    А в активити мы регистрируем ресивер, а в onDestroy отписываем его.

--- app/src/main/java/com/example/kotlin_lesson_1/view/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_lesson_1/view/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.kotlin_lesson_1.view
 
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlin_lesson_1.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val myBCReceiver = MySystemBroadcastReceiver()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,5 +25,15 @@ class MainActivity : AppCompatActivity() {
                 )
                 .commit()
         }
+
+        registerReceiver(myBCReceiver, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
+        registerReceiver(myBCReceiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+
     }
+
+    override fun onDestroy() {
+        unregisterReceiver(myBCReceiver)
+        super.onDestroy()
+    }
+
 }
