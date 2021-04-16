0e8203a4b207f2986aa676737097f5353221accd
GromZDev <Dmitryev@mail.ru>
2021-04-17 02:31:35 +0300

    В активити сделал меню верхнее с кнопкой, по которой мы и входим во фрагмент, отображающий из БД список истории просмотра фильмов.

--- app/src/main/java/com/example/kotlin_lesson_1/view/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_lesson_1/view/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlin_lesson_1.R
+import com.example.kotlin_lesson_1.view.filmHistory.FilmHistoryFragment
 import com.example.kotlin_lesson_1.view.main.MainFilmFragment
 
 class MainActivity : AppCompatActivity() {
@@ -35,6 +38,26 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         unregisterReceiver(myBCReceiver)
         super.onDestroy()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_screen_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_history -> {
+                supportFragmentManager.apply {
+                    beginTransaction()
+                        .replace(R.id.fragment_container, FilmHistoryFragment.newInstance())
+                        .addToBackStack("")
+                        .commitAllowingStateLoss()
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
