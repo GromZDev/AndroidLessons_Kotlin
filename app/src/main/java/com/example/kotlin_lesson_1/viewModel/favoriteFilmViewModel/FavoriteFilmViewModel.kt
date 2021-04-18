885c5f66aabf4051536332486473922345140282
GromZDev <Dmitryev@mail.ru>
2021-04-18 03:24:04 +0300

    Написал новый sealed class AppStateFavorite для отображения состоний загрузки и показа списка Любимых фильмов, которые сохраняются в отдельной БД.
    Также сделал новый класс FavoriteFilmViewModel вкупе с лив-датой для нового фрагмента отображения посещенных фильмов. Сдесь же и сечу новый репозиторий RoomFavoriteLocalRepository.

---
+++ app/src/main/java/com/example/kotlin_lesson_1/viewModel/favoriteFilmViewModel/FavoriteFilmViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.kotlin_lesson_1.viewModel.favoriteFilmViewModel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.kotlin_lesson_1.app.App
+import com.example.kotlin_lesson_1.repository.roomFavoriteRepository.RoomFavoriteLocalRepository
+import com.example.kotlin_lesson_1.repository.roomFavoriteRepository.RoomFavoriteLocalRepositoryImpl
+import com.example.kotlin_lesson_1.viewModel.AppStateFavorite
+
+class FavoriteFilmViewModel(
+    val favoriteFilmLiveData: MutableLiveData<AppStateFavorite> = MutableLiveData(),
+    private val filmFavoriteRepository: RoomFavoriteLocalRepository =
+        RoomFavoriteLocalRepositoryImpl(App.getFilmFavoriteDao())
+) : ViewModel() {
+    fun getAllFavoriteFilms() {
+        favoriteFilmLiveData.value = AppStateFavorite.Loading
+        Thread {
+            Thread.sleep(1000)
+            favoriteFilmLiveData.postValue(AppStateFavorite.Success(filmFavoriteRepository.getAllFavorite()))
+        }.start()
+    }
+}
