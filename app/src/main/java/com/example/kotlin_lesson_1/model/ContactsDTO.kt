e935c5fe8433885ffc5ea274936e3b5201def44d
GromZDev <Dmitryev@mail.ru>
2021-04-19 15:56:21 +0300

    Для вывода Контактов телефона сделал отдельный класс ContactsDTO, который хранит в себе имя, номер и картинку.
    Для вывода информации сделал recycler и адаптер во фрагменте.
    Теперь выводим имя, номер и картинку (если есть, если нет, то она у нас null).

---
+++ app/src/main/java/com/example/kotlin_lesson_1/model/ContactsDTO.kt
@@ -0,0 +1,10 @@
+package com.example.kotlin_lesson_1.model
+
+import android.graphics.Bitmap
+
+class ContactsDTO {
+
+    var name = ""
+    var number: String? = ""
+    var image : Bitmap? = null
+}
