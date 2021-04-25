c9d8aa9ba8afa216e896376df3c4435d56569cbb
GromZDev <Dmitryev@mail.ru>
2021-04-25 04:06:57 +0300

    Сделал новый класс для сервиса по приёму Push-нотификаций, и для него в манифесте прописал интент, по которому он будет вызываться. Когда приложение свёрнуто, то уведомления будут приходить и с "пустым" классом MyPushNotificationService. P.S.: пуш-сообщение на этом этапе приходит! Пришло через 1.5 минуты )

---
+++ app/src/main/java/com/example/kotlin_lesson_MyMovieApp/view/MyPushNotificationService.kt
@@ -0,0 +1,6 @@
+package com.example.kotlin_lesson_MyMovieApp.view
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyPushNotificationService : FirebaseMessagingService() {
+}
